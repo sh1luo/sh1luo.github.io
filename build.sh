1b81ac596b1293e1e7369c448b280b40fc8d7053
sh1luo <690898835@qq.com>
2021-09-07 16:35:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 rm -rf ./public ./docs && 
 hugo &&
 echo kcode.icu >> ./public/CNAME && 
-mv public docs &&
-git add . && 
-git commit -m "update blog..." &&
-git push origin master
+mv public docs
